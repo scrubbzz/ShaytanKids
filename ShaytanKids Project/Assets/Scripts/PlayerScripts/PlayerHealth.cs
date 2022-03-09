d05c6b573ee3a81e794f710cc6da72c9fe4cdcbb
Hasan Ahmed Abdalla <hasanhasan.molair@gmail.com>
2022-03-09 10:29:44 +0400

    Player now regenerates.
    If not damaged for a few seconds the player will start regenerating and this will be displayed via the health bar during run time.

--- ShaytanKids Project/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ ShaytanKids Project/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int currentHealth;
+    public float currentHealth;
     public int maxHealth;
 
     public HealthBarManager healthBarManager;
+
+    public int healthRegenTimer = 3;
+    public float currentHealthRegenTimer;
+    public bool tookDamage;
+    public int regenRate;
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = 100;
         currentHealth = maxHealth;
         healthBarManager.SetMaxHealth(maxHealth);
+
+        currentHealthRegenTimer = healthRegenTimer;
     }
 
     // Update is called once per frame
@@ -22,11 +29,41 @@ public class PlayerHealth : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             TakeDamage(20);
+            tookDamage = true;
+            currentHealthRegenTimer = healthRegenTimer;
         }
+        StartRegenTimer();
+        RegenerateHealth();
+        healthBarManager.playerHealth.value = currentHealth;
     }
     private void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBarManager.SetHealth(currentHealth);
+    }
+    public void StartRegenTimer()
+    {
+        if (tookDamage)
+        {
+            currentHealthRegenTimer -= Time.deltaTime;
+        }
+       
+        if(currentHealthRegenTimer <= 0)
+        {
+            currentHealthRegenTimer = 0;
+        }
+    }
+    public void RegenerateHealth()
+    {
+        if(tookDamage && currentHealthRegenTimer == 0)
+        {
+            currentHealth += regenRate * Time.deltaTime;
+            if(currentHealth >= maxHealth)
+            {
+                currentHealth = maxHealth;
+                currentHealthRegenTimer = healthRegenTimer;
+                tookDamage = false;
+            }
+        }
     }
 }
