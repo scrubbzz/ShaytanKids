7de6ecb1807605a4eb72c7563e6595ef5e9bea23
Hasan Ahmed Abdalla <hasanhasan.molair@gmail.com>
2022-03-09 10:24:07 +0400

    Archangels now attack
    created the enemy functionality that will allow the archangels to shoot projectiles towards the player if he is within a certain range

---
+++ ShaytanKids Project/Assets/Scripts/EnemyScripts/Archangels/ArchangelAttackManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArchangelAttackManager : MonoBehaviour, IAffectPlayer
+{
+    public int projectileDamage;
+
+    public GameObject Target;
+    public Transform detectorOrigin;
+    public Vector2 detectorSize = new Vector2(30, 30);
+    public Vector2 detectorOriginOffset = Vector2.zero;
+
+    public Vector2 directionToTarget;
+    public LayerMask detectorLayermask;
+
+    public float timer;
+    public float fireDelay;
+
+    public GameObject projectile;
+    public int projectileSpeed;
+    public Vector2 spawnOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        detectorOrigin = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        LocatePlayer();
+        DamagePlayer();
+    }
+    public void LocatePlayer()
+    {
+        Collider2D collider = Physics2D.OverlapBox((Vector2)detectorOrigin.position + detectorOriginOffset, detectorSize, 0, detectorLayermask);
+        if(collider != null)
+        {
+            Target = collider.gameObject;
+            Debug.Log("DetectedPlayer");
+        }
+        else
+        {
+            Target = null;
+        }
+    }
+    public void DamagePlayer()
+    {
+        directionToTarget = Target.transform.position - detectorOrigin.position;
+      
+        if(timer >= fireDelay)
+        {
+            GameObject theProjectile = Instantiate(projectile, transform.position + transform.TransformDirection(spawnOffset), Quaternion.identity);
+            Rigidbody2D rb = theProjectile.GetComponent<Rigidbody2D>();
+
+            Vector2 direction = directionToTarget * projectileSpeed; ;
+            rb.velocity = direction;
+            rb.transform.up = direction;
+
+            timer = 0;
+        }
+      
+    }
+   
+   
+}
