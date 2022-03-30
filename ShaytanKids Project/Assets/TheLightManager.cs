9062a20299a25e22d919e6ba5a0d8495a3178f2d
Hasan Ahmed Abdalla <hasanhasan.molair@gmail.com>
2022-03-30 16:54:15 +0400

    (Ignore me)

--- ShaytanKids Project/Assets/TheLightManager.cs
+++ ShaytanKids Project/Assets/TheLightManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Rendering.Universal;
 public class TheLightManager : MonoBehaviour
 {
     Light2D playerLight;
+    public float lightLossAmount;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,15 @@ public class TheLightManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DecreaseLight();
+    }
+    public void IncreaseLight()
+    {
+
+    }
+    public void DecreaseLight()
+    {
+        playerLight.pointLightInnerRadius -= lightLossAmount * Time.deltaTime;
+        playerLight.pointLightOuterRadius -= lightLossAmount * Time.deltaTime;
     }
 }
