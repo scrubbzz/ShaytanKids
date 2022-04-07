b321e3c57371cc2fb0a97aa505205d848525775c
Mamadou <m.hussein@saedubai.com>
2022-04-07 13:00:49 +0400

    Added shaytan kid attack, chase, idle and fixed animation bugs

---
+++ ShaytanKids Project/Assets/Kid.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kid : MonoBehaviour
+{
+    Transform target;
+    public Transform borderCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector2(-1, 1);
+
+        }
+    }
+}
